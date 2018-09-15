5b9e97b4559e852a67f17088614f4e1eb3ab8380
Fernando Osegueda <oseguedah@gmail.com>
2018-09-14 23:39:40 -0600

    Correción en las vistas

--- src/main/java/sv/com/nipro/interfaz/InterfazApplication.java
+++ src/main/java/sv/com/nipro/interfaz/InterfazApplication.java
@@ -1,15 +1,45 @@
 package sv.com.nipro.interfaz;
 
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.context.ServletContextAware;
+
+import com.sun.faces.config.ConfigureListener;
 
 @SpringBootApplication(scanBasePackages={"sv.com.nipro.interfaz.controller"})
 @EnableJpaAuditing
-public class InterfazApplication {
+public class InterfazApplication implements ServletContextAware{
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterfazApplication.class, args);
+	}
+	
+	@Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+            new FacesServlet(), "*.xhtml");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(
+            new ConfigureListener());
+    }
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+		
 	}
 
 }
